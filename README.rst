2f485f77b695ff742346fe25b76595045147ae20
Carl Waldbieser <cwaldbieser@gmail.com>
2014-09-25 21:54:21 -0400

    Added initial files for the proxy.

---
+++ README.rst
@@ -0,0 +1,6 @@
+=================
+Twisted CAS Proxy
+=================
+
+An HTTPS proxy that sits in front of a web application and provides CAS 
+authentication as a service.
---
+++ txcasproxy/txcasproxy.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python
+
+#Standard library
+import os.path
+
+#External modules
+import jinja2
+from klein import Klein
+import treq
+#from twisted.internet.defer import inlineCallbacks
+from twisted.web.static import File
+
+class ShopSite(object):
+    app = Klein()
+
+    def __init__(self):
+        template_path = os.path.join(os.path.dirname(__file__), "templates")
+        self.loader = jinja2.FileSystemLoader(searchpath=template_path)
+        self.template_env = jinja2.Environment(loader=self.loader)
+
+    @app.route("/static/", branch=True)
+    def static(self, request):
+        return File("./static")          
+
+    @app.route("/", branch=True)
+    def proxy(self, request):
+        """
+        """
+        kwds = {}
+        kwds['headers'] = dict(request.requestHeaders.getAllRawHeaders())
+        if request.method in ('PUT', 'POST'):
+            kwds['data'] = request.content.read()
+        print "kwds", kwds
+        d = treq.request(request.method, 'http://parkland.tlcdelivers.com:8080' + request.uri, **kwds)
+        def process_headers(response):
+            for k,v in response.headers.getAllRawHeaders():
+                request.responseHeaders.setRawHeaders(k, v)
+            return response
+        d.addCallback(process_headers)
+        d.addCallback(treq.content)
+        return d
+    
+
+if __name__ == "__main__":
+    #Run standalone.
+    import sys
+    from twisted.python import log
+    log.startLogging(sys.stdout)
+ 
+    store = ShopSite()
+    store.app.run('127.0.0.1', 8080)
+else:
+    #Or with `twistd -y`
+    from twisted.application import internet
+    from twisted.application.service import Application
+    from twisted.web.server import Site
+
+    store = ShopSite()
+    resource = store.app.resource()
+
+    application = Application("txShop")
+    service = internet.TCPServer(8080, Site(resource))
+    service.setServiceParent(application)
+
